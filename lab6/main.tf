16ac7d1e0843c257609902e431cae042037308ab
James Delbridge <jamesdelbridge@yahoo.co.uk>
2024-12-10 16:05:29 +0000

    Lab 6

--- lab6/main.tf
+++ lab6/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.18.0"
     }
   }
@@ -9,7 +9,23 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
 }
+
+terraform {
+  cloud {
+
+    organization = "JDelbridge_Test"
+
+    workspaces {
+      name = "my-first-workspace"
+    }
+  }
+}
+
 
 resource "azurerm_resource_group" "lab6" {
   name     = "RG1"
@@ -28,6 +44,6 @@ resource "azurerm_subnet" "lab6" {
   name                 = "test-subnet"
   resource_group_name  = azurerm_resource_group.lab6.name
   virtual_network_name = azurerm_virtual_network.lab6.name
-  address_prefixes      = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
